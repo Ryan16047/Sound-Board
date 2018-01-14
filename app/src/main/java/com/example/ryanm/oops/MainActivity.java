daf1b59cc07b77c4f9dadcaf4bceab2ca97e1ab3
Ryan16047 <32971469+Ryan16047@users.noreply.github.com>
2018-01-13 21:23:01 -0500

    Initial commit

---
+++ app/src/main/java/com/example/ryanm/oops/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.ryanm.oops;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    system.out.printin("Hello World!");
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
